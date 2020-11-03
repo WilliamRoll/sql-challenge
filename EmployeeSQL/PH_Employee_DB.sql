1ca06090fb777f46d2ab6a1f83fafd97cc5c5058
WilliamRoll <broll6595@gmail.com>
2020-11-03 18:28:03 -0500

    tables created, csv files uploaded successfully

--- EmployeeSQL/PH_Employee_DB.sql
+++ EmployeeSQL/PH_Employee_DB.sql
@@ -1,7 +1,7 @@
 -- departments table
 CREATE TABLE departments (
 	dept_no VARCHAR(20) PRIMARY KEY NOT NULL,
-	dept_name VARCHAR(50) NOT NULL,
+	dept_name VARCHAR(50) NOT NULL
 );
 
 -- dept_emp table
@@ -18,3 +18,25 @@ CREATE TABLE dept_manager (
 	PRIMARY KEY (dept_no, emp_no)
 );
 
+--employees table
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(20) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(50) NOT NULL, 
+	last_name VARCHAR(50) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL
+);
+
+--salaries table
+CREATE TABLE salaries (
+	emp_no INT PRIMARY KEY NOT NULL,
+	salary INT NOT NULL
+);
+
+--titles table
+CREATE TABLE titles (
+	title_id VARCHAR(20) PRIMARY KEY NOT NULL,
+	title VARCHAR(30) NOT NULL
+);

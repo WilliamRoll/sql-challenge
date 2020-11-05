9e771babcc9451fdbccb6e589d0ffad17882d6db
WilliamRoll <broll6595@gmail.com>
2020-11-04 20:06:26 -0500

    First query updated. 2nd question answered and query working.

--- EmployeeSQL/PH_Employee Queries.sql
+++ EmployeeSQL/PH_Employee Queries.sql
@@ -1,9 +1,16 @@
 SELECT * FROM employees
 SELECT * FROM salaries
 --list employee #, LN, FN, Sex, Salary for each employee
-SELECT employees.emp_no, employees.first_name, employees.last_name, employees.sex, salaries.salary
+SELECT employees.emp_no AS Employee_ID
+	,employees.first_name AS Employee_First_Name
+	,employees.last_name AS Employee_Last_Name
+	,employees.sex AS Employee_Sex
+	,salaries.salary AS Employee_Salary
 FROM employees
-LEFT JOIN salaries
+INNER JOIN salaries
 ON employees.emp_no = salaries.emp_no
 
---
+--list FN, LN, hire date for employees hired in 1986
+SELECT first_name, last_name, hire_date 
+FROM employees 
+WHERE DATE_PART('YEAR', hire_date) = 1986;

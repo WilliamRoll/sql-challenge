99dbd80a606d3f39f7dc39f98d870b5440e76e6b
WilliamRoll <broll6595@gmail.com>
2020-11-04 20:19:21 -0500

    3rd question query created and outputting correct result

--- EmployeeSQL/PH_Employee Queries.sql
+++ EmployeeSQL/PH_Employee Queries.sql
@@ -1,5 +1,8 @@
 SELECT * FROM employees
 SELECT * FROM salaries
+SELECT * FROM dept_manager
+SELECT * FROM departments
+SELECT * FROM dept_emp
 --list employee #, LN, FN, Sex, Salary for each employee
 SELECT employees.emp_no AS Employee_ID
 	,employees.first_name AS Employee_First_Name
@@ -14,3 +17,14 @@ ON employees.emp_no = salaries.emp_no
 SELECT first_name, last_name, hire_date 
 FROM employees 
 WHERE DATE_PART('YEAR', hire_date) = 1986;
+
+--list manager of each dept w/ the following info: dept #, dept name, manager's  employee #, LN, FN
+SELECT dm.dept_no AS Department_ID
+	,dept.dept_name AS Department_Name
+	,dm.emp_no AS Manager_ID
+	,emp.last_name AS Manager_Last_Name
+	,emp.first_name AS Manager_First_Name
+FROM dept_manager AS dm
+INNER JOIN departments AS dept ON dept.dept_no = dm.dept_no
+INNER JOIN employees AS emp ON emp.emp_no = dm.emp_no
+

9e42f9523d0f312a792b1cfd91cc7fd522c42fab
WilliamRoll <broll6595@gmail.com>
2020-11-07 16:02:34 -0500

    Updated table queries and database diagram

---
+++ EmployeeSQL/PH_Create_Table_Queries.sql
@@ -0,0 +1,47 @@
+-- departments table
+CREATE TABLE departments (
+	dept_no VARCHAR(20) PRIMARY KEY NOT NULL,
+	dept_name VARCHAR(50) NOT NULL
+);
+
+--titles table
+CREATE TABLE titles (
+	title_id VARCHAR(20) PRIMARY KEY NOT NULL,
+	title VARCHAR(30) NOT NULL
+);
+
+--employees table
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(20) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(50) NOT NULL, 
+	last_name VARCHAR(50) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date DATE NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+);
+
+-- dept_emp table
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(20) NOT NULL,
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+-- debt_manager table
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(20) NOT NULL,	
+	emp_no INT NOT NULL,
+	PRIMARY KEY (dept_no, emp_no), 
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+--salaries table
+CREATE TABLE salaries (
+	emp_no INT PRIMARY KEY NOT NULL,
+	salary INT NOT NULL
+);
